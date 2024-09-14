9b8a4c2038a322424af54719750cbe39f581deb7
Kaizen86 <danielbridgewater87@gmail.com>
2024-09-14 22:15:17 +0100

    Merge shell.nix's

--- shell.nix
+++ shell.nix
@@ -2,7 +2,12 @@
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = with pkgs.buildPackages; [
+      # main.py dependencies
       python311Packages.pyserial
+
+      # Pico development dependencies
+      cmake
+      gcc-arm-embedded
     ];
 }
 
