56470235d77de2efdaa3c167a9afa4d3205c0646
Kaizen86 <danielbridgewater87@gmail.com>
2024-09-15 23:58:19 +0100

    Overhaul controller script
    Refactored config_lora by creating serial_send and serial_recv functions
    
    Added automatic decoding of radio module errors
    
    Check responses from the setup commands to make sure OK is being returned
    
    Added a spinner to indicate waiting after setup

--- main.py
+++ main.py
@@ -1,40 +1,102 @@
 #!/usr/bin/env python3
 from serial import Serial
 from time import sleep
+from sys import stdout
 
-def config_lora(port, command):
+def serial_recv(port):
+    if port.in_waiting:
+        # Format response(s)
+        recv = port.read_all().decode('ascii').split('\n')
+        recv = [s.strip() for s in recv if s.strip() != '']
+        return recv
+    else:
+        return []
+
+def serial_send(port, command):
     # Send command
     port.flush()
     print('-->', command)
     port.write(f"{command}\r\n".encode('ascii'))
+
     # Wait for response
-    while not port.in_waiting:
+    for i in range(30):
+        recv = serial_recv(port)
+        if len(recv) > 0:
+            break
         sleep(0.2)
+    else:
+        recv = [] 
+
     # Print response(s)
-    recv = port.read_all().decode('ascii').split('\n')
-    recv = [s.strip() for s in recv if s.strip() != '']
     for message in recv:
         print('<--', message)
+    
+    return recv
+
+radio_error_codes = {'+ERR='+str(k):v for k,v in {
+    1: "There is not “enter” or 0x0D 0x0A in the end of the AT Command.",
+    2: "The head of AT command is not “AT” string.",
+    3: "There is not “=” symbol in the AT command.",
+    4: "Unknown command.",
+    10: "TX is over times.",
+    11: "RX is over times.",
+    12: "CRC error.",
+    13: "TX data more than 240 bytes.",
+    15: "Unknown error."
+}.items()}
+
+def config_lora(port, command):
+    # Clear input buffer
+    #port.read_all()
+    # Send command and wait for response
+    recv = serial_send(port, command)
+    # Check if there was an error
+    for line in recv:
+        if "ERR" in line:
+            # If there was, kill the program and display the error meaning
+            raise Exception("Radio module error: "+radio_error_codes[line])
+    # Otherwise return the received data
+    return recv
+
 
 with Serial("/dev/ttyACM0", 9600) as port:
     # Setup LoRa radio module
     # https://reyax.com//upload/products_download/download_file/LoRa%20AT%20Command%20RYLR40x_RYLR89x_EN.pdf
-    port.read_all()
-    config_lora(port, "AT")
-    config_lora(port, "AT+PARAMETER=12,7,1,4") # Set LoRa parameters
-    config_lora(port, "AT+BAND=868500000") # 868.5 MHz (Europe license-free band)
-    config_lora(port, "AT+MODE=0") # Disable sleep mode
-    config_lora(port, "AT+ADDRESS=86")
-    config_lora(port, "AT+NETWORKID=3")
-    config_lora(port, "AT+CRFOP=00")
+    commands = [
+        "AT", # Check if the module is connected & ready
+        "AT+PARAMETER=12,7,1,4", # Set LoRa parameters
+        "AT+BAND=868500000", # 868.5 MHz (Europe license-free band)
+        "AT+MODE=0", # Disable sleep mode
+        "AT+NETWORKID=3",
+        "AT+ADDRESS=86",
+        "AT+CRFOP=00" # Output power in dBm (00-15)
+    ]
+
+    for command in commands:
+        response = config_lora(port, command)
+        if len(response) > 1:
+            print("More than one response... weird")
+        for line in response:
+            if line != "+OK":
+                raise Exception("AA something broke")
+
     print("Setup complete")
 
+    spinner = 0
     while True:
-        if port.in_waiting:
-            recv = port.read_all().decode('ascii').split('\n')
-            recv = [s.strip() for s in recv if s.strip() != '']
-            for message in recv:
-                print("<--", message)
-                if message.startswith("+RCV"):
-                    print("wowie a message for us!")
+        recv = serial_recv(port)
+        if recv:
+            # Overtype spinner
+            print('\r', end='')
+        for message in recv:
+            if message.startswith("+RCV"):
+                print("wowie a message for us!")
+            print("<--", message)
+
+        animation = "|/─\\"
+        spinner += 1
+        if spinner >= len(animation):
+            spinner = 0
+        print('\r '+animation[spinner], end=' ')
+        stdout.flush()
         sleep(1)
